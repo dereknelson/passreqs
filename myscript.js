4fc1f7fc36a95f171a5e4bd26cce3d92a93d7401
Jack <jack_sherbal@my.uri.edu>
2017-12-01 16:01:33 -0500

    cleanup

--- myscript.js
+++ myscript.js
@@ -1,17 +1,10 @@
-// console.log($);
-console.log('you\'re in the world of content.js');
-
-// console.log($(':password').is(':focus'));
-
 var hasFocus = false;
 
 $(':password').on('focus', function () {
     console.log($(this).parent());
     if(!$(this).parent().is('#tooltip')) {
-        console.log($(this));
         $(this).wrap('<div id="tooltip" data-tip="testing tooltip"></div>');
         this.focus();
-        console.log('password box has focus');
         hasFocus = true;
     }
 });
@@ -19,30 +12,13 @@ $(':password').on('focus', function () {
 $(':password').focusout(function () {
     setTimeout(function () {
         var focus = $(document.activeElement);
-        console.log(focus);
-        if(focus.is('#tooltip')) {
-            console.log('test');
-        }
         if (focus.is(':password') || $(':password').has(focus).length) {
             console.log("still focused");
         } else {
-            // console.log(".focusout");
             $(':password').unwrap();
         }
     }, 1000);
-    console.log('focus lost');
     hasFocus = false;
 });
 
-// var ans = {};
-//
-// ans.createSidebar = function() {
-//
-//     return {
-//         init: function(){
-//             console.log('init func');
-//         }
-//     }
-// }();
-//
-// ans.createSidebar.init();
+console.log(window.location.hostname);
