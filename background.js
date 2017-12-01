5de3765a123421437de4ce8b6b54b4e5b768cfc9
Jack <jack_sherbal@my.uri.edu>
2017-12-01 14:44:52 -0500

    password tooltip added

--- background.js
+++ background.js
@@ -5,8 +5,11 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 
         chrome.tabs.executeScript(null, { file: "node_modules/jquery/dist/jquery.js" }, function() {
             console.log('jQuery loaded');
-            chrome.tabs.executeScript(null, { file: "myscript.js" }, function (result) {
-                console.log('content loaded')
+            chrome.tabs.insertCSS(null, {file: "tooltip.css"}, function () {
+                console.log('css loaded');
+                chrome.tabs.executeScript(null, { file: "myscript.js" }, function (result) {
+                    console.log('content loaded')
+                });
             });
         });
     }
--- myscript.js
+++ myscript.js
@@ -1,14 +1,48 @@
-console.log($);
+// console.log($);
 console.log('you\'re in the world of content.js');
-var ans = {};
 
-ans.createSidebar = function() {
+// console.log($(':password').is(':focus'));
 
-    return {
-        init: function(){
-            alert("why hello there");
-        }
+var hasFocus = false;
+
+$(':password').on('focus', function () {
+    console.log($(this).parent());
+    if(!$(this).parent().is('#tooltip')) {
+        console.log($(this));
+        $(this).wrap('<div id="tooltip" data-tip="testing tooltip"></div>');
+        this.focus();
+        console.log('password box has focus');
+        hasFocus = true;
     }
-}();
+});
 
-ans.createSidebar.init();
+$(':password').focusout(function () {
+    setTimeout(function () {
+        var focus = $(document.activeElement);
+        console.log(focus);
+        if(focus.is('#tooltip')) {
+            console.log('test');
+        }
+        if (focus.is(':password') || $(':password').has(focus).length) {
+            console.log("still focused");
+        } else {
+            // console.log(".focusout");
+            $(':password').unwrap();
+        }
+    }, 1000);
+    console.log('focus lost');
+    hasFocus = false;
+});
+
+// var ans = {};
+//
+// ans.createSidebar = function() {
+//
+//     return {
+//         init: function(){
+//             console.log('init func');
+//         }
+//     }
+// }();
+//
+// ans.createSidebar.init();
