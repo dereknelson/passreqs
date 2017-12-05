bfb2e665d8913cd093c0134612b6cde75086374e
Jack <jack_sherbal@my.uri.edu>
2017-12-05 14:51:33 -0500

    WOOOO we fuckin functional now boi

--- myscript.js
+++ myscript.js
@@ -1,18 +1,28 @@
 var hasFocus = false;
 
 var xhttp = new XMLHttpRequest();
+var that = this;
 xhttp.onreadystatechange = function () {
     if(this.readyState == 4 && this.status == 200) {
-        console.log(xhttp.responseText);
+        // console.log(xhttp.responseText);
+        that.pass_reqs = JSON.parse(xhttp.response);
+        console.log(that.pass_reqs);
     }
 };
 xhttp.open("GET", "http://127.0.0.1:8000/api/passreqs/?format=json&"+ window.location.hostname +"=", true);
 xhttp.send();
 
+var that = this;
 $(':password').on('focus', function () {
     console.log($(this).parent());
     if(!$(this).parent().is('#tooltip')) {
-        $(this).wrap('<div id="tooltip" data-tip="testing tooltip"></div>');
+        console.log(that.pass_reqs[0]);
+        var string = "Password requirements for " + window.location.hostname + " are; \n" +
+            "Minimum Length: " + that.pass_reqs[0].length + "\n" +
+            "Capital Required: " + Boolean(parseInt(that.pass_reqs[0].capitals)) + "\n" +
+            "Special Characters Required: " + Boolean(parseInt(that.pass_reqs[0].special_characters)) + "\n" +
+            "Complexity Required: " + Boolean(parseInt(that.pass_reqs[0].complex));
+        $(this).wrap('<div id="tooltip" data-tip="'+ string +'"></div>');
         this.focus();
         hasFocus = true;
     }
